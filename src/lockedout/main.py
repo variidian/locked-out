b8674283c38d7a8b94016d6f4c37891a05928391
turdle <shuruiguo1@gmail.com>
2026-03-12 17:22:40 +1100

    guh

--- src/lockedout/main.py
+++ src/lockedout/main.py
@@ -78,9 +78,9 @@ with shelve.open(mydata) as db: #check if its time to remind the user as long as
             with open(lockinArt, 'r', encoding="utf-8") as file:
                 content = file.read()
                 print(content)
-            webbrowser.open_new_tab(str(Path(script_dir)/'lockin.html'))
-            playsound(Path(script_dir) / "alarm.mp3")
             with shelve.open(mydata) as db:
                 del db['savedReminderTime'] 
+            webbrowser.open_new_tab(str(Path(script_dir)/'lockin.html'))
+            playsound(Path(script_dir) / "alarm.mp3")
             break
         time.sleep(30)
